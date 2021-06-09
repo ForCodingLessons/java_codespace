3549849f451d765bcf26250c96fdec33cc0b91c0
ForCodingLessons <85629606+ForCodingLessons@users.noreply.github.com>
2021-06-09 17:39:36 +0300

    Create Task12.java

---
+++ lesson1/Task12.java
@@ -0,0 +1,12 @@
+package com.alfabank.work12;
+import java.util.Scanner;
+
+public class Task12 {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the name");
+        String line = sc.nextLine();
+        System.out.println("My name is " + line);
+    }
+}
